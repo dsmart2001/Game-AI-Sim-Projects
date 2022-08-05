f6ba2736fdc4520b6ba88ef42b8818663a231700
Dallas Smart <dsmart10@outlook.com>
2022-08-05 10:52:24 +1000

    File Setup and small edits

--- AI_SIM_SOBH/Assets/Scripts/Agent AI/AIBrain.cs
+++ AI_SIM_SOBH/Assets/Scripts/Agent AI/AIBrain.cs
@@ -1,12 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Panda;
+using System.Linq;
+
+[RequireComponent(typeof(AIAiming))]
+[RequireComponent(typeof(AIAttacks))]
+[RequireComponent(typeof(AIMovement))]
 
 public class AIBrain : MonoBehaviour
 {
-    //[RequireComponent(typeof(AIAiming))]
-    //[RequireComponent(typeof(AIAttacks))]
-    //[RequireComponent(typeof(AIMovement))]
+    public enum AIBehaviours
+    {
+        Idle,
+        Roam,
+        Attack,
+        Forage,
+        Flee
+    }
+
+    public AIBehaviours behaviours;
+
+    // Agent health and state values
+    public float health;
+    private float maxHealth;
+    public string agentName;
+
+    // Agent movement and nav values
+    public float speed;
+    private float rotationSpeed = 5.0f;
+    
+    public GameObject target;
+    private Quaternion targetRotation;
+    public float targetRadius = 150f;
+
+    // Agent attack variables
+    private Vector3 attackPos;
+    public GameObject attackProjectile;
+    private float attackTimer;
+    public float attackFireRate = 4f;
+
+    // Agent flee variables
+    private Vector3 fleePos;
+
+    // Agent forage variables
+    private Vector3 foragePos;
+    public List<GameObject> foundPickUps;
+    public List<GameObject> usedPickups;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +58,28 @@ public class AIBrain : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // BEHAVIOUR: Roam, move between random patrol points
+    [Task]
+    public void Roam()
+    {
+
+    }
+
+    [Task]
+    public void Attack()
+    {
+
+    }
+
+    [Task] public void Flee()
+    {
+
+    }
+
+    [Task] public void Forage()
+    {
+
     }
 }
